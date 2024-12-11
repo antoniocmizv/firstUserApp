8d65474b0c4dc05fdac25e9586e0b49b4b222523
Antonio Castillo <acasmor1311@ieszaidinvergeles.org>
2024-12-11 12:47:58 +0000

    subida

--- app/Http/Controllers/AdministratorController.php
+++ app/Http/Controllers/AdministratorController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Http\Request;
 
 class AdministratorController extends Controller
 {
     //
+    function __construct()
+    {
+        $this->middleware(AdminMiddleware::class);
+    }
 }
